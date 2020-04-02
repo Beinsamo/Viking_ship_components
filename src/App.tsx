58e57a01052e48e35d7c0d4920bcfe605c38a306
刘宇坤 <liuyukun@liuyukundeMBP.fritz.box>
2020-04-02 15:58:16 +0200

    add-icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React,{useState} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas} from '@fortawesome/free-solid-svg-icons'
+
+
 import Button, {ButtonSize,ButtonType} from './components/Button/button';
 import Alert, {AlertType} from './components/Alert/alert';
-
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuitem'
 import SubMenu from './components/Menu/submenu'
+import Icon from './components/Icon/icon'
 
 function App() {
   let [open,openAlert]=useState(false)
-
+  library.add(fas)
   return (
     <div className="App">
       <header className="App-header">
+       <Icon icon='arrow-down' theme='danger' size='8x' />
        <Button size={ButtonSize.Large} btnType={ButtonType.Primary} onClick={()=>{openAlert(true)}}>nihao</Button>        
        <Alert idName='1' altType={AlertType.Success} title={'alertsucess'} content='it sucesses' />
 
